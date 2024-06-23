2a85992d8d0c1669a4545b193cf8c569a0cb907e
Rodrigo <qwiklabs@cloudon.com.br>
2024-06-23 21:20:07 +0000

    ..

--- devai-jira-ui-qwiklabs/src/resolvers/index.js
+++ devai-jira-ui-qwiklabs/src/resolvers/index.js
@@ -5,13 +5,16 @@ const resolver = new Resolver();
 resolver.define('getText', (req) => {
   console.log(req);
 
-  resolver.define('getApiKey', (req) => {
-    return process.env.LLM_API_KEY;
-  });
-  
-  resolver.define('getDevAIApiUrl', (req) => {
-    return process.env.DEVAI_API_URL;
-  });
+  return 'Hello world!';
+
+});
+
+resolver.define('getApiKey', (req) => {
+  return process.env.LLM_API_KEY;
+});
+
+resolver.define('getDevAIApiUrl', (req) => {
+  return process.env.DEVAI_API_URL;
 });
 
 export const handler = resolver.getDefinitions();
